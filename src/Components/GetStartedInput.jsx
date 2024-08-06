84bdeb195b015ea1919c0195505b30fb4c4d5f3d
Blessing Olusanya <debbyblessing1234@gmail.com>
2024-08-06 06:13:23 -0700

    Add functionality to the form input

--- src/Components/GetStartedInput.jsx
+++ src/Components/GetStartedInput.jsx
@@ -2,10 +2,28 @@ import React from "react";
 import LoginForm from "./Input";
 
 const GetStartedInput = () => {
-  
+
+  const [lastName, SetLastName] = useState('');
+  const [fistName, SetFirstName] = useState('');
+
+  const handleInput = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'lastName') {
+      SetLastName(value);
+    }  else if (name === 'fistName') {
+      SetFirstName(value); // Set firstname value
+  }}
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('LastName:', lastName);
+    console.log('FirstName:', firstName);
+  };
+
     return(
     <>
-    <div className="input-btn-container">
+    <div className="input-btn-container" onSubmit={handleSubmit}>
             <div className="lastname-container">
          <label className="lastname">First name</label>
          <input 
@@ -13,6 +31,7 @@ const GetStartedInput = () => {
          id="lastname"
          className="lastname-input"
           name="lastname" 
+          onChange={handleInput}
             value=''
             required
             />
@@ -24,13 +43,12 @@ const GetStartedInput = () => {
          id="lastname"
          className="lastname-input"
           name="lastname" 
+          onChange={handleInput}
             value=''
             required
             />
             </div>
-            <LoginForm />
-            
-            
+            <LoginForm />    
     </div>
     </>
   )
